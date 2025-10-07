0232576ab83da0298d8c368e482619760fd0b7d4
Karina Klages <karinaklages@web.de>
2025-10-07 18:49:10 +0200

    refactor: JSON structure added

---
+++ scripts/data.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        "question": "Welcher Baum ist in Mitteleuropa am weitesten verbreitet?",
+        "answer_1": "Buche",
+        "answer_2": "Eiche",
+        "answer_3": "Fichte",
+        "answer_4": "Kiefer",
+        "right_answer": "Fichte",
+    },
+    {
+        "question": "Wie nennt man eine Gruppe von Wölfen?",
+        "answer_1": "Rudel",
+        "answer_2": "Herde",
+        "answer_3": "Schwarm",
+        "answer_4": "Kolonie",
+        "right_answer": "Rudel",
+    },
+    {
+        "question": "Welches Tier hält keinen Winterschlaf?",
+        "answer_1": "Igel",
+        "answer_2": "Eichhörnchen",
+        "answer_3": "Dachs",
+        "answer_4": "Fledermaus",
+        "right_answer": "Eichhörnchen",
+    },
+    {
+        "question": "Wie nennt man den Lebensraum, in dem Bäume die dominierende Pflanzenform sind?",
+        "answer_1": "Wüste",
+        "answer_2": "Savanne",
+        "answer_3": "Wald",
+        "answer_4": "Tundra",
+        "right_answer": "Wald",
+    },
+    {
+        "question": "Welches Tier ist das größte lebende Säugetier der Erde?",
+        "answer_1": "Afrikanischer Elefant",
+        "answer_2": "Blauwal",
+        "answer_3": "Pottwal",
+        "answer_4": "Giraffe",
+        "right_answer": "Blauwal",
+    }
+];
